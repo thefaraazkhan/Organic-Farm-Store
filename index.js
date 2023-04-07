13fb828406e831d4fbfc05a74de52091e353679d
thefaraazkhan <khanmohdfaraaz@gmail.com>
2023-04-07 20:31:33 +0530

    Added views and models

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 //Adding mongoose
 const mongoose = require('mongoose');
-// const Product = require('./models/product');
+const Product = require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -16,9 +16,28 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}))
 
-app.get('/dogs', (req,res) => {
-    res.send('Woof');
+app.get('/products', async (req,res) => {
+    const products = await Product.find({});
+    // console.log(products);
+    res.render('products/index', { products });
+})
+
+app.get('/products/new', (req,res) => {
+    res.render('products/new');
+})
+
+app.post('/products', async (req,res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`products/${newProduct._id}`);
+})
+
+app.get('/products/:id', async (req,res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
 })
 
 app.listen(3000, () => {
